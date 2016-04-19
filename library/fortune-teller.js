8162c7a1dd5fa9953182594136fb83fd32ca5991
Charles Gaudreau Jackson <gaudreaujacksoncharles@gmail.com>
2016-04-19 16:50:08 -0400

    ...

---
+++ library/fortune-teller.js
@@ -0,0 +1,3 @@
+var fortune = require('fortune');
+
+fortune.getFortune();
--- library/fortune.js
+++ library/fortune.js
@@ -2,11 +2,13 @@ function randBetween(a, b) {
  return Math.floor(Math.random() * (b - a + 1)) + a;
 }
 
-function getFortune(str) {
+function getFortune() {
   var quotes = ["The future smiles at you.", "You will soon find your soulmate.", "You will find yourself soon enough."];
+  var randNumber = randBetween(1,5);
   console.log(str);
 }
 
 module.exports = {
+  randBetween: randBetween,
   getFortune: getFortune
 };
