847ec4c6f9b5dda8c2bbb3fb24386ed8f1072eac
Charles Gaudreau Jackson <gaudreaujacksoncharles@gmail.com>
2016-04-19 11:47:37 -0400

    ...

---
+++ call-callbacks.js
@@ -0,0 +1,13 @@
+// Create a function called firstChar that takes a string and a callback, and "returns" the first character of the string after one second.
+// NOTE: You won't be allowed to use the return keyword, because you'll only be "returning" in the callback to setTimeout, way after your function has finished executing.
+// Create a function called lastChar that takes a string and "returns" the last character of the string after one second.
+// Create a function called getFirstAndLast that takes a string and "returns" the first+last character of the string. Your function should use firstChar and lastChar to do its work. I should be able to call your function like this:
+
+
+function firstChar(str) {
+  setTimeout(function(str) {
+    console.log(str);
+  },1000);
+}
+
+firstChar("Hello");
