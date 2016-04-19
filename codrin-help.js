586942b894ba13e784389a724e857a9b917d2efb
Charles Gaudreau Jackson <gaudreaujacksoncharles@gmail.com>
2016-04-19 15:42:50 -0400

    call-callbacks

--- codrin-help.js
+++ codrin-help.js
@@ -3,39 +3,13 @@
 // Create a function called lastChar that takes a string and "returns" the last character of the string after one second.
 // Create a function called getFirstAndLast that takes a string and "returns" the first+last character of the string. Your function should use firstChar and lastChar to do its work. I should be able to call your function like this:
 
-var sampleString = "Charles is learning callstack";
-
-function firstChar(str, callback) {
-  var firstCharacter = str[0];
+function first(str, callback) {
+  var firstCharacter = str.charAt(0);
   setTimeout(function() {
     callback(firstCharacter);
   },1000);
 }
 
-firstChar(sampleString, function(str) {
-  console.log(str);
-});
-
-
-function lastChar(str, callback) {
-  var lastCharacter = str.charAt(str.length - 1);
-  setTimeout(function() {
-    callback(lastCharacter);
-  },1000);
-}
-
-lastChar(sampleString, function(str) {
-  console.log(str);
-});
-
-function getFirstAndLast(str, callback) {
-  firstChar(str, function(firstLetter) {
-    lastChar(str, function(lastLetter) {
-        callback(firstLetter + " " + lastLetter);
-    });
-  });
-}
-
-getFirstAndLast(sampleString, function(str) {
-  console.log(str);
+first("Hello", function(str) {
+  return str;
 });
